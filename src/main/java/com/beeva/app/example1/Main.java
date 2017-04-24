6cec23124fffcf486f0fd8a5da3ea9efb86abb0d
Oscar Juarez <elmagnificosm@gmail.com>
2017-04-24 16:07:50 -0500

    Se quitan las dependencias con Calculadora

--- src/main/java/com/beeva/app/example1/Main.java
+++ src/main/java/com/beeva/app/example1/Main.java
@@ -1,21 +1,11 @@
 package com.beeva.app.example1;
 
-import org.beeva.com.calculadora.Calculadora;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		String operacion= new String(args[0]);
-		Integer a= new Integer(args[1]);
-		Integer b= new Integer(args[2]);
-		Calculadora calc = CalculadoraInstance.getInstance();
-		int resultado;
-		if (operacion.equals("suma")){
-			resultado=calc.suma(a,b);			
-		}
-		else {
-			resultado=calc.resta(a,b);
-		}
+		String resultado="hola";
 		System.out.println(resultado);
 
 	}
